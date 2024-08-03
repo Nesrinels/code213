e0334b90f4c3784c8b29f858bc9c20f7c9a832b5
rbtsoheib <taharsoheib08@gmail.com>
2024-08-03 14:12:23 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Sidebarright from './Sidebarright';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App/> */}
+    <Sidebarright/>
   </React.StrictMode>
 );
 
