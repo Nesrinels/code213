7975227c7055df88e88159a416698c0a567eaced
Nesrinels <lounissi.nesrine@gmail.com>
2024-08-17 12:44:45 +0100

    feed part 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Header from './component/header/Header';
+import Feed from './component/feed/Feed';
 function App() {
   return (
     <>
     <Header/>
+    <Feed />
     </>
   )
 }
