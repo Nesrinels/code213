5fa1ba0dd7e4f3872e5243eb29c974d1ed9518b5
rbtsoheib <taharsoheib08@gmail.com>
2024-08-30 11:55:43 +0200

    finished Sidebarright events : jsx / css

--- src/component/Sidebarright/Suggest.js
+++ src/component/Sidebarright/Suggest.js
@@ -5,99 +5,92 @@ import user3 from "../../assets/images/user3.png";
 import user4 from "../../assets/images/user2.png";
 import user1 from "../../assets/images/user1.png";
 import user2 from "../../assets/images/user4.png";
-import user5 from "../../assets/images/user-pic-1.png"
-import user6 from "../../assets/images/user-pic-2.png"
-import user7 from "../../assets/images/user-pic-3.png"
+import user5 from "../../assets/images/user-pic-1.png";
+import user6 from "../../assets/images/user-pic-2.png";
+import user7 from "../../assets/images/user-pic-3.png";
 
 function Suggest() {
   return (
     <>
-
-
       <div className="firstComponent">
-      {/* the first small div  --> start  */}
+        {/* the first small div  --> start  */}
 
-      <div className="firstSmallDiv">
-        <h4>Your Team</h4>
+        <div className="firstSmallDiv">
+          <h4>Your Team</h4>
 
-        <a href="#S">See all</a>
+          <a href="#S">See all</a>
 
-        <hr></hr>
-      </div>
-
-      {/* the first small div  --> end */}
-
-      {/* second small div --> start  */}
-
-      <div className="secondSmallDiv">
-        {/* first micro div --> start  */}
-
-        <div className="firstmicroDiv">
-          <img src={user5} alt="user1"></img>
+          <hr></hr>
         </div>
 
-        {/* first micro div --> end  */}
+        {/* the first small div  --> end */}
 
-        {/* second micro div --> start  */}
+        {/* second small div --> start  */}
 
-        <div className="secondmicroDiv">
-          <h4>Anthony Daugloi</h4>
+        <div className="secondSmallDiv">
+          {/* first micro div --> start  */}
 
-          <h5>12 mutual friends</h5>
+          <div className="firstmicroDiv">
+            <img src={user5} alt="user1"></img>
+          </div>
+
+          {/* first micro div --> end  */}
+
+          {/* second micro div --> start  */}
+
+          <div className="secondmicroDiv">
+            <h4>Anthony Daugloi</h4>
+
+            <h5>12 mutual friends</h5>
+          </div>
+
+          {/* second micro div --> end  */}
         </div>
 
-        {/* second micro div --> end  */}
-      </div>
+        {/* second small div --> end */}
 
-      {/* second small div --> end */}
+        {/* third small div --> start  */}
 
-      {/* third small div --> start  */}
+        <div className="thirdSmallDiv">
+          {/* first micro div --> start  */}
 
-      <div className="thirdSmallDiv">
-        {/* first micro div --> start  */}
+          <div className="firstMicroDiv">
+            <img src={user6} alt="user2"></img>
+          </div>
 
-        <div className="firstMicroDiv">
-          <img src={user6} alt="user2"></img>
+          {/* first micro div --> end  */}
+
+          {/* second micro div --> start  */}
+
+          <div className="secondMicroDiv">
+            <h4>Mohannad Zitoun</h4>
+
+            <h5>12 mutual friends</h5>
+          </div>
+
+          {/* second micro div --> end  */}
         </div>
 
-        {/* first micro div --> end  */}
+        {/* third small div --> end  */}
 
-        {/* second micro div --> start  */}
+        {/* fourth small div --> start  */}
 
-        <div className="secondMicroDiv">
+        <div className="fourthSmallDiv">
+          {/* first micro div --> start  */}
 
-          <h4>Mohannad Zitoun</h4>
+          <div className="FirstmicroDiv">
+            <img src={user7} alt="user3"></img>
+          </div>
 
-          <h5>12 mutual friends</h5>
+          {/* first micro div --> end  */}
 
-        </div>
+          <div className="SecondmicroDiv">
+            <h4>Mohannad Zebar</h4>
 
-        {/* second micro div --> end  */}
-      </div>
-
-      {/* third small div --> end  */}
-
-      {/* fourth small div --> start  */}
-
-      <div className="fourthSmallDiv">
-        {/* first micro div --> start  */}
-
-        <div className="FirstmicroDiv">
-          <img src={user7} alt="user3"></img>
-          
-        </div>
-
-        {/* first micro div --> end  */}
-
-        <div className="SecondmicroDiv">
-          <h4>Mohannad Zebar</h4>
-
-          <h5>12 mutual friends</h5>
+            <h5>12 mutual friends</h5>
+          </div>
         </div>
       </div>
-    </div>
-
-
 
       <div className="thirdpart">
         <div className="suppart">
@@ -123,45 +116,44 @@ function Suggest() {
         </div>
       </div>
 
-      <div className='lastpart'>
-      <div className='suppart2'>
-      <h4>Event</h4>
-      <a href="#S" > See all</a>
+      <div className="lastpart">
+        <div className="suppart2">
+          <h4>Event</h4>
+          <a href="#S"> See all</a>
+        </div>
+        <div className="A2">
+          <div className="a">
+            FEB <br />
+            22
+          </div>
+          <div className="b">
+            <h4>Meeting with clients</h4>
+            <p>41 madison ave, floor 24 new york, NY 10010</p>
+          </div>
+        </div>
+        <div className="B2">
+          <div className="c">
+            APR <br />
+            30
+          </div>
+          <div className="d">
+            <h4>Developer Program</h4>
+            <p>41 madison ave, floor 24 new york, NY 10010</p>
+          </div>
+        </div>
+        <div className="C2">
+          <div className="e">
+            APR <br />
+            23
+          </div>
+          <div className="f">
+            <h4>Developer Program</h4>
+            <p>41 madison ave, floor 24 new york, NY 10010</p>
+          </div>
+        </div>
       </div>
-      <div className='A2'>
-        <div className='a'>
-          FEB <br />22
-        </div>
-        <div className='b'>
-          <h4>Meeting with clients</h4>
-          <p>41 madison ave, floor 24 new york, NY 10010</p>
-        </div>
-      </div>
-      <div className='B'>
-        <div className='c'>
-          APR <br />30
-        </div>
-        <div className='d'>
-          <h4>Developer Program</h4>
-          <p>41 madison ave, floor 24 new york, NY 10010</p>
-        </div>
-      </div>
-      <div className='C'>
-        <div className='e'>
-          APR <br />23
-        </div>
-        <div className='f'>
-          <h4>Developer Program</h4>
-          <p>41 madison ave, floor 24 new york, NY 10010</p>
-        </div>
-      </div>
-
-
-    </div>
-
-</>
+    </>
   );
 }
-
 
 export default Suggest;
