1bade23efa447551057a820f2186fdcb414f9952
rbtsoheib <taharsoheib08@gmail.com>
2024-08-30 17:41:49 +0200

    hhhhhhhhhhhh

--- src/component/Sidebarright/Suggest.js
+++ src/component/Sidebarright/Suggest.js
@@ -1,15 +1,41 @@
 import "./Suggest.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from ''
 import image from "../../assets/images/image.jpg";
 import user3 from "../../assets/images/user3.png";
 import user4 from "../../assets/images/user2.png";
 import user1 from "../../assets/images/user1.png";
 import user2 from "../../assets/images/user4.png";
+import user5 from "../../assets/images/user-pic-1.png";
+import user6 from "../../assets/images/user-pic-2.png";
+import user7 from "../../assets/images/user-pic-3.png"
+
 
 function Suggest() {
+
   return (
     <>
+
+<div className="suggest">
+      <div className="block1">
+        <h4>Suggest groupes</h4>
+        <a href="#S">See all</a>
+      </div>
+
+      <div className="block2">
+        <hr id="hr" />
+        <img src={image} alt="grp Suggestion" />
+      </div>
+
+      <div className="block3">
+        <img  src={user1} alt="user1" />
+        <img  src={user2} alt="user2" />
+        <img  src={user3} alt="user3" />
+        <img  src={user4} alt="user4" />
+        <a href="#S"><p>+2</p></a>
+        <p>Member apply</p>
+      </div>
+    </div>
+
       <div className="firstComponent">
         {/* the first small div  --> start  */}
 
