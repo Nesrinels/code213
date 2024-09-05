0832bc6f5c501b834a4502890a6493033663c8f5
rbtsoheib <taharsoheib08@gmail.com>
2024-09-05 19:56:18 +0200

    login jsx and css / needs some modifications

---
+++ src/component/login/login.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./login.css";
+
+function login() {
+
+return (
+
+<>
+<div class="wrapper">
+        <form>
+
+            <h1>Log in </h1>
+            <div className="input-box">
+
+                <input type="text" placeholder="username" required />
+
+                <i className='bx bxs-user'></i>
+
+            </div>
+            <div className="input-box">
+
+                <input type="password" placeholder="password" required />
+
+                <i className='bx bxs-lock-alt'></i>
+
+            </div>
+            <div className="input-box">
+
+                <input type="email" placeholder="email" required />
+
+                <i className='bx bx-envelope'></i>
+
+            </div>
+
+            <div className="Remember-forgot">
+
+                <label><input type="checkbox" />Remember me</label>
+
+            </div>
+
+            <button type="submit" className="btn"> LET ME IN !</button>
+
+        </form>
+    </div>
+</>
+
+);
+
+
+}
+
+export default login;
