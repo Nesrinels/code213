5c984b947cd0849699574756ba927a7746b22678
Nesrinels <lounissi.nesrine@gmail.com>
2024-09-05 16:32:18 +0100

    group

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 
+// import './App.css';
+// import Header from './component/header/Header';
+// import Sidebarleft from './component/Sidebarleft/Sidebarleft';
+// import Container from './Container/Container';
+// import Groups from './Groups/Groups';
+
+
+// function App() {
+//   return (
+//     <>
+//     <Header/>
+//     <Container>
+   
+//       <Sidebarleft/>
+//     </Container>
+//     <Groups/>
+//     </>
+//   )
+// }
+
+// export default App;
+
+
 import './App.css';
 import Header from './component/header/Header';
 import Sidebarleft from './component/Sidebarleft/Sidebarleft';
 import Container from './Container/Container';
 import Groups from './Groups/Groups';
 
-
 function App() {
   return (
-    <>
-    <Header/>
-    <Container>
-   
-      <Sidebarleft/>
-    </Container>
-    <Groups/>
-    </>
+    <div className="app">
+      <Header />
+      <Container>
+        <div className="main-content">
+          <Sidebarleft />
+          <div className="center-content">
+            <Groups />
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
